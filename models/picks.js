6a508129684dbe4bdc8d9811ddab4eb21ac0c2e2
Gabe Baduqui <baduquig@gmail.com>
2023-01-13 21:41:59 -0700

    Create Pick Model. Controllers and Routes WIP...

---
+++ models/picks.js
@@ -0,0 +1,46 @@
+import mongoose from "mongoose";
+
+const pickSchema = mongoose.Schema({
+    // Pick related data
+    _id: {
+        userId: Number,
+        gameId: Number
+    },
+    userName: String,
+    schoolPicked: {
+        type: Number,
+        default: ''
+    },
+    scoreDifferential: {
+        type: String,
+        default: ''
+    },
+    points: {
+        type: String,
+        default: 0
+    },
+    
+    // Game related data
+    week: Number,
+    day: Number,
+    date: String,
+    gameTime: Date,
+    finalScore: String,
+    awaySchoolId: Number,
+    awaySchoolName: String,
+    awaySchoolMascot: String,
+    homeSchoolId: Number,
+    homeSchoolName: String,
+    homeSchoolMascot: String,
+
+    // Timestamps
+    createdDateTime: {
+        type: Date,
+        default: new Date()
+    },
+    lastUpdatedDateTime: Date
+})
+
+const Pick = mongoose.model('Pick', pickSchema);
+
+export default Pick;
